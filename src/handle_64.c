5df7e82a5b2083c4900db48379cfe094f72134f8
thifranc <thifranc@student.42.fr>
2017-10-30 18:41:11 +0100

    store n_sects in struct -> not well tested

--- src/handle_64.c
+++ src/handle_64.c
@@ -6,7 +6,7 @@
 /*   By: thifranc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/23 17:41:38 by thifranc          #+#    #+#             */
-/*   Updated: 2017/10/30 17:12:00 by thifranc         ###   ########.fr       */
+/*   Updated: 2017/10/30 18:37:48 by thifranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,24 +51,39 @@ int		symtab_64(struct symtab_command *sc, char *ptr, char ***all_string)
 	return (0);
 }
 
+int		get_n_sect(int nsects, char *segname, t_a *g, struct section_64 *sec_64)
+{
+	int		j;
+	char	*sectname;
+
+	j = 0;
+	while (j < nsects)
+	{
+		sectname = sec_64[j].sectname;
+		if (ft_strcmpi(segname, sectname) == 0)
+		{
+			if (!ft_strcmp(sectname, "__data"))
+				g->data_sec = g->n_sect + j + 1;
+			else if (!ft_strcmp(sectname, "__text"))
+				g->text_sec = g->n_sect + j + 1;
+			else if (!ft_strcmp(sectname, "__bss"))
+				g->bss_sec = g->n_sect + j + 1;
+		}
+		j++;
+	}
+	g->n_sect += j;
+	return (0);
+}
+
 int		get_n_sect64(struct segment_command_64* sg ,t_a *g)
 {
 	struct	section_64	*sec_64;
-	char *segname;
-	char *sectname;
-	int	j;
+	char	*segname;
 
 	segname = sg->segname;
 	sec_64 = (void*)sg + sizeof(struct segment_command_64);
 
-	j = 0;
-	while (j < (int)sg->nsects)
-	{
-		sectname = sec_64[j].sectname;
-		printf("segname = %s && sectname ==> %s\n", segname, sectname);
-		j++;
-	}
-	g->n_sect += j;
+	get_n_sect((int)sg->nsects, segname, g, sec_64);
 	return (0);
 }
 
@@ -85,6 +100,7 @@ int		handle_64(char *title, char *ptr, t_a g)
 	lc = (void *)ptr + sizeof(struct mach_header_64);
 
 	i = 0;
+	g.n_sect = 0;
 	while (i < (int)header->ncmds)
 	{
 		if (lc->cmd == LC_SEGMENT_64)
@@ -93,11 +109,11 @@ int		handle_64(char *title, char *ptr, t_a g)
 		}
 		if (lc->cmd == LC_SYMTAB)
 		{
+			dprintf(1, "sections are : bss %d | text %d | data %d \n", (int)g.bss_sec, (int)g.text_sec, (int)g.data_sec);
 			symtab_64((struct symtab_command *)lc, ptr, &output);
 		}
 		lc = (void *)lc + lc->cmdsize;
 		i++;
 	}
 	return (0);
-
 }
